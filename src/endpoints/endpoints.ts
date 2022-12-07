b7d165d80ac40c57639f68ce532bc1093dc79470
MaxD <max.moderation@gmail.com>
2022-12-07 19:52:56 +0200

    feat: change style in welcome page & root url for new deploy

--- src/endpoints/endpoints.ts
+++ src/endpoints/endpoints.ts
@@ -1,4 +1,5 @@
-const URL = 'https://dt-lang-server.herokuapp.com'
+// const URL = 'https://dt-lang-server.herokuapp.com'
+const URL = 'https://rslang-be-rebuild-service.onrender.com'
 
 const Endpoints = {
   ROOT: `${URL}`,
--- src/ui/ifFromDictionary/goToAuthButton.tsx
+++ src/ui/ifFromDictionary/goToAuthButton.tsx
@@ -14,4 +14,5 @@ function GoToAuthButton() {
 
   )
 }
+
 export default GoToAuthButton
